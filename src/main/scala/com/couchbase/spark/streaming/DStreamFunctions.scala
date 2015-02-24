671af1496d5f357deeb917400ff7a5477831db14
zsxwing <zsxwing@gmail.com>
2015-02-24 19:33:47 +0800

    Call DocumentRDDFunctions.saveToCouchbase directly

--- src/main/scala/com/couchbase/spark/streaming/DStreamFunctions.scala
+++ src/main/scala/com/couchbase/spark/streaming/DStreamFunctions.scala
@@ -1,10 +1,8 @@
 package com.couchbase.spark.streaming
 
 import com.couchbase.client.java.document.Document
-import com.couchbase.spark.connection.{CouchbaseBucket, CouchbaseConnection, CouchbaseConfig}
+import com.couchbase.spark._
 import org.apache.spark.streaming.dstream.DStream
-import rx.lang.scala.JavaConversions._
-import rx.lang.scala.Observable
 
 
 class DStreamFunctions[D <: Document[_]](dstream: DStream[D]) extends Serializable {
@@ -12,21 +10,7 @@ class DStreamFunctions[D <: Document[_]](dstream: DStream[D]) extends Serializab
   def sparkContext = dstream.context.sparkContext
 
   def saveToCouchbase(bucketName: String = null): Unit = {
-    val cbConfig = CouchbaseConfig(sparkContext.getConf)
-
-    dstream.foreachRDD { rdd =>
-      rdd.foreachPartition { iter =>
-        if (iter.nonEmpty) {
-          val bucket = CouchbaseConnection().bucket(bucketName, cbConfig).async()
-          Observable
-            .from(iter.toIterable)
-            .flatMap(doc => toScalaObservable(bucket.upsert[D](doc)))
-            .toBlocking
-            .last
-        }
-      }
-    }
-
+    dstream.foreachRDD(_.saveToCouchbase(bucketName))
   }
 
 
