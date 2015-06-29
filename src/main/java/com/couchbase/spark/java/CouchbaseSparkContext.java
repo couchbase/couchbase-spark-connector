9a6efae3c85b2b37e8f87dd32c7afe83fb652039
Michael Nitschinger <michael@nitschinger.at>
2015-06-29 17:59:19 +0200

    move java files where they belong

--- src/main/java/com/couchbase/spark/java/CouchbaseSparkContext.java
+++ src/main/java/com/couchbase/spark/java/CouchbaseSparkContext.java
@@ -1,3 +1,24 @@
+/**
+ * Copyright (C) 2015 Couchbase, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALING
+ * IN THE SOFTWARE.
+ */
 package com.couchbase.spark.java;
 
 import com.couchbase.client.java.document.Document;
@@ -14,8 +35,6 @@ import com.couchbase.spark.rdd.SpatialViewRDD;
 import com.couchbase.spark.rdd.ViewRDD;
 import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaRDD;
-import scala.collection.Seq;
-import scala.reflect.ClassTag;
 import java.util.List;
 
 public class CouchbaseSparkContext {
@@ -44,9 +63,12 @@ public class CouchbaseSparkContext {
 
     @SuppressWarnings({"unchecked"})
     public <D extends Document> JavaRDD<D> couchbaseGet(List<String> ids, String bucket, Class<D> clazz) {
-        ClassTag<D> classTag = scala.reflect.ClassTag$.MODULE$.apply(clazz);
-        Seq<String> idSeq = scala.collection.JavaConversions.asScalaBuffer(ids).seq();
-        return new KeyValueRDD(sc, idSeq, bucket, classTag).toJavaRDD();
+        return new KeyValueRDD(
+            sc,
+            CouchbaseUtil.listToSeq(ids),
+            bucket,
+            CouchbaseUtil.classTag(clazz)
+        ).toJavaRDD();
     }
 
     public JavaRDD<CouchbaseViewRow> couchbaseView(final ViewQuery query) {
---
+++ src/test/java/com/couchbase/spark/java/CouchbaseSparkContextTest.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (C) 2015 Couchbase, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALING
+ * IN THE SOFTWARE.
+ */
+package com.couchbase.spark.java;
+
+import com.couchbase.client.java.document.JsonDocument;
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.junit.Test;
+import java.util.Arrays;
+
+import static com.couchbase.spark.java.CouchbaseSparkContext.javaFunctions;
+
+public class CouchbaseSparkContextTest {
+
+    @Test
+    public void shouldGetADocument() {
+        SparkConf conf = new SparkConf().setAppName("javaTest").setMaster("local[*]");
+        SparkContext sc = new SparkContext(conf);
+        CouchbaseSparkContext csc = javaFunctions(sc);
+
+        JavaRDD<JsonDocument> docs = csc.couchbaseGet(Arrays.asList("airline_2357"));
+
+        System.out.println(docs.collect());
+    }
+
+}
