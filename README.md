97699c0437c595c9807cbe5eb9057bf25f49247d
Michael Nitschinger <michael@nitschinger.at>
2016-05-24 07:04:06 +0200

    Readme & License Cleanup.
    Change-Id: I9ca68e279c2dd9e2cd22ff75016c269286219ddd
    Reviewed-on: http://review.couchbase.org/64325
    Reviewed-by: Michael Nitschinger <michael@nitschinger.at>
    Tested-by: Michael Nitschinger <michael@nitschinger.at>

--- README.md
+++ README.md
@@ -19,11 +19,24 @@ libraryDependencies += "com.couchbase.client" %% "spark-connector" % "1.1.0"
 
 If you need to build for Spark 1.4, use the `1.0.x` connector version.
 
+## Version Compatibility
+
+Each minor release is targeted for a specific spark version and once released
+branched away. Couchbase maintains bugfix releases for the branches where
+appropriate, please see [Maven Central](http://search.maven.org/#search%7Cga%7C1%7Ccom.couchbase.client.spark) 
+or [Spark Packages](http://spark-packages.org/package/couchbase/couchbase-spark-connector) for releases to download.
+
+| Connector | Spark |
+| --------- | ----- |
+| 1.2.x (upcoming) | 1.6   |
+| 1.1.x            | 1.5   |
+| 1.0.x            | 1.4   |
+
 ## Documentation
-The official documentation, including a quickstart guide can be found [here](http://developer.couchbase.com/documentation/server/4.0/connectors/spark-1.0/getting-started.html).
+The official documentation, including a quickstart guide can be found [here](http://developer.couchbase.com/documentation/server/4.0/connectors/spark-1.0/spark-intro.html).
 
 ## License
-Copyright 2015 Couchbase Inc.
+Copyright 2015,2016 Couchbase Inc.
 
 Licensed under the Apache License, Version 2.0.
 
--- src/main/scala/com/couchbase/spark/rdd/ViewRDD.scala
+++ src/main/scala/com/couchbase/spark/rdd/ViewRDD.scala
@@ -1,23 +1,17 @@
-/**
- * Copyright (C) 2015 Couchbase, Inc.
+/*
+ * Copyright (c) 2015 Couchbase, Inc.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALING
- * IN THE SOFTWARE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.couchbase.spark.rdd
 
@@ -28,17 +22,17 @@ import com.couchbase.client.core.time.Delay
 import com.couchbase.client.java.error.{CouchbaseOutOfMemoryException, TemporaryFailureException}
 import com.couchbase.client.java.util.retry.RetryBuilder
 import com.couchbase.client.java.view.ViewQuery
-import com.couchbase.spark.connection.{CouchbaseConnection, CouchbaseConfig}
+import com.couchbase.spark.connection.{CouchbaseConfig, CouchbaseConnection}
 import com.couchbase.spark.internal.LazyIterator
-import org.apache.spark.{TaskContext, Partition, SparkContext}
+import org.apache.spark.{Dependency, Partition, SparkContext, TaskContext}
 import org.apache.spark.rdd.RDD
-
 import rx.lang.scala.JavaConversions._
 
 case class CouchbaseViewRow(id: String, key: Any, value: Any)
 
-class ViewRDD(@transient sc: SparkContext, viewQuery: ViewQuery, bucketName: String = null)
-  extends RDD[CouchbaseViewRow](sc, Nil) {
+class ViewRDD(@transient sc: SparkContext, viewQuery: ViewQuery, bucketName: String = null,
+              deps: Seq[Dependency[_]])
+  extends RDD[CouchbaseViewRow](sc, deps) {
 
   private val cbConfig = CouchbaseConfig(sc.getConf)
 
@@ -76,5 +70,5 @@ class ViewRDD(@transient sc: SparkContext, viewQuery: ViewQuery, bucketName: Str
 
 object ViewRDD {
   def apply(sc: SparkContext, bucketName: String, viewQuery: ViewQuery) =
-    new ViewRDD(sc, viewQuery, bucketName)
+    new ViewRDD(sc, viewQuery, bucketName, Nil)
 }
