24509910458565c7fdcc6747b7b82af80ec05a0b
Joel LaFall <joellafall@gmail.com>
2018-01-12 10:40:27 -0800

    SPARKC-86 Fix CouchbaseSink so it can use Spark state management for aggregations
    Change-Id: I8204debd2e902383f10af3b081bb74df4dfc3af8
    Reviewed-on: http://review.couchbase.org/87945
    Reviewed-by: David Nault <david.nault@couchbase.com>
    Tested-by: David Nault <david.nault@couchbase.com>

--- src/main/scala/com/couchbase/spark/sql/streaming/CouchbaseSink.scala
+++ src/main/scala/com/couchbase/spark/sql/streaming/CouchbaseSink.scala
@@ -18,7 +18,15 @@ package com.couchbase.spark.sql.streaming
 import com.couchbase.spark.Logging
 import org.apache.spark.sql.{DataFrame, SaveMode}
 import org.apache.spark.sql.execution.streaming.Sink
+import org.apache.spark.unsafe.types.UTF8String
+import org.apache.spark.sql.types.StringType
 import com.couchbase.spark.sql._
+import com.couchbase.spark._
+import com.couchbase.client.core.CouchbaseException
+import com.couchbase.client.java.document.JsonDocument
+import com.couchbase.client.java.document.json.JsonObject
+import scala.concurrent.duration._
+
 
 /**
   * A simple Structured Streaming sink which writes the data frame to the bucket.
@@ -31,7 +39,30 @@ import com.couchbase.spark.sql._
 class CouchbaseSink(options: Map[String, String]) extends Sink with Logging {
 
   override def addBatch(batchId: Long, data: DataFrame): Unit = {
-    data.write.mode(SaveMode.Overwrite).couchbase(options)
+    val bucketName = options.get("bucket").orNull
+    val idFieldName = options.getOrElse("idField", DefaultSource.DEFAULT_DOCUMENT_ID_FIELD)
+    val removeIdField = options.getOrElse("removeIdField", "true").toBoolean
+    val timeout = options.get("timeout").map(v => Duration(v.toLong, MILLISECONDS))
+
+    data.toJSON
+      .queryExecution
+      .toRdd
+      .map(_.get(0, StringType).asInstanceOf[UTF8String].toString())
+      .map { rawJson =>
+          val encoded = JsonObject.fromJson(rawJson)
+          val id = encoded.get(idFieldName)
+
+          if (id == null) {
+              throw new Exception(s"Could not find ID field $idFieldName in $encoded")
+          }
+
+          if (removeIdField) {
+              encoded.removeKey(idFieldName)
+          }
+
+          JsonDocument.create(id.toString, encoded)
+      }
+      .saveToCouchbase(bucketName, StoreMode.UPSERT, timeout)
   }
 
 }
