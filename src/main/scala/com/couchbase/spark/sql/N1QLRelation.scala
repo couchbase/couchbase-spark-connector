8057143f172f57fd534da2809b9af67f1be05753
Paul Bauer <p2bauer@users.noreply.github.com>
2020-10-19 14:16:05 +1000

    Allow passthrough of timestampFormat format for inferring schema

--- src/main/scala/com/couchbase/spark/sql/N1QLRelation.scala
+++ src/main/scala/com/couchbase/spark/sql/N1QLRelation.scala
@@ -72,8 +72,16 @@ class N1QLRelation(bucket: String, userSchema: Option[StructType], parameters: M
       .map(_.value.toString)
     val dataset = sqlContext.sparkSession.createDataset(rdd)(Encoders.STRING)
 
+    val timestampFormat = if (parameters.get("timestampFormat").isDefined) {
+      parameters("timestampFormat")
+    } else {
+      // use the default in Spark 2.3 FastDateParser
+      "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
+    }
+
     val schema = sqlContext
       .read
+      .option("timestampFormat", timestampFormat)
       .json(dataset)
       .schema
 
